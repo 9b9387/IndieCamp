a043b556a8f59489483b124dfad2c85db50bacab
谭锦华 <kinnka.tan@gmail.com>
2017-12-09 14:50:16 +0800

    -- 添加主场景、动画资源

---
+++ Assets/Scripts/Test/TestCharator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HandyEvent;
+
+public class TestCharator : MonoBehaviour {
+
+	NavAgent agent;
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<NavAgent> ();
+		EventManager.Instance.AddListener (HandyEvent.EventType.player_action, OnPlayerAction);
+	}
+	
+	void OnPlayerAction(EventArgs args){
+		StandardInputType type = args.GetValue<StandardInputType> ();
+		if (type == StandardInputType.mouse_left_button_down){
+			Vector2 pos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+			agent.SetDestination (pos);
+		}
+	}
+}
